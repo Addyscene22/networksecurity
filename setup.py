4f56b3b3b256df0f2c145ca49a771342d93ba692
Addyscene22 <palsuleadwait@gmail.com>
2025-07-12 01:21:58 +0530

    installed required libraries

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements() -> List[str]:
+    requirement_lst: List[str] = []
+    try:
+        with open('requirements.txt', 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                requirement = line.strip()
+                # Ignore empty lines and '-e .'
+                if requirement and requirement != '-e .':
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt not found")
+    return requirement_lst
+
+setup(
+    name='Network-security',
+    version='0.0.1',
+    author="Adwait-Palsule",
+    author_email='palsuleadwait@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
