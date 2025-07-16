6722acf34cd426168f5b1766aac84460aca4add7
Addyscene22 <palsuleadwait@gmail.com>
2025-07-17 01:17:23 +0530

    added data validation

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 import sys 
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 if __name__ =="__main__":
@@ -11,8 +12,18 @@ if __name__ =="__main__":
         dataingestionconfig = DataIngestionConfig(trainingpipelineconfig)
         data_ingestion = DataIngestion(dataingestionconfig)
         logging.info("initiate the data ingestion")
+        
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
-        print(dataingestionconfig)
+        logging.info("Data initiation comppleted")
+        print(dataingestionartifact)
+        datavalidationconfig = DataValidationConfig(trainingpipelineconfig)
+        data_validation = DataValidation(dataingestionartifact,datavalidationconfig)
+        logging.info("Initiate the data validation config")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info("data vlaidation Completed")
+        print(data_validation_artifact)
 
+       
+        
     except Exception as e:
         raise NetworkSecurityException(e, sys)
--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -4,3 +4,13 @@ from dataclasses import dataclass
 class DataIngestionArtifact:
     trained_file_path:str
     test_file_path:str
+
+@dataclass
+class DataValidationArtifact:
+    validation_status: bool
+    validated_train_file_path: str = None
+    validated_test_file_path: str = None
+    invalid_train_file_path: str = None
+    invalid_test_file_path: str = None
+    drift_report_file_path: str = None
+    message: str = ""
