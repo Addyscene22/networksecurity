20c908968e7f9b2e5e231ce7c72c572016807224
Addyscene22 <palsuleadwait@gmail.com>
2025-07-21 23:42:24 +0530

    setup for model training

--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -20,3 +20,15 @@ class DataTransformationArtifact:
     transformed_object_file_path : str
     trained_train_file_path : str
     test_train_file_path: str
+
+@dataclass
+class ClassificationMetricArtifact:
+    f1_score: float
+    precision_score: float
+    recall_score: float
+    
+@dataclass
+class ModelTrainerArtifact:
+    trained_model_file_path: str
+    train_metric_artifact: ClassificationMetricArtifact
+    test_metric_artifact: ClassificationMetricArtifact
